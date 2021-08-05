34432905cf87deb084b8b0c6678f64ae04c66969
USER <hongyi578@gmail.com>
2021-08-05 22:38:14 +0800

    登入跳轉修改

--- app/src/main/java/com/example/carapp/Login.java
+++ app/src/main/java/com/example/carapp/Login.java
@@ -11,24 +11,40 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 import android.content.Intent;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
     private Button btn_login;
+    private EditText  username=null;
+    private EditText  password=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btn_login =(Button) findViewById(R.id.btn_login);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        username = (EditText) findViewById(R.id.account);
+        password = (EditText) findViewById(R.id.pwd);
         btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Toast.makeText(Login.this, "登入成功", Toast.LENGTH_SHORT).show();
+                login();
+            }
+        });
+    }
+        public void login(){
+            if (username.getText().toString().equals("123") &&
+                    password.getText().toString().equals("123")) {
+                Toast.makeText(getApplicationContext(), "登入成功",
+                        Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(Login.this,MainActivity.class);
                 startActivity(intent);
                 finish();
+            } else {
+                Toast.makeText(getApplicationContext(), "登入失敗",
+                        Toast.LENGTH_SHORT).show();
             }
-        });
+        }
 
 
-    }
 }
